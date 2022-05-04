dd2a686f7caed8498065085e0ebe23f9a4dd43ba
Buwar88 <buwar_emerica@hotmail.com>
2022-05-04 00:33:26 -0500

    Agregando server con endpoints

--- app/lib/server.js
+++ app/lib/server.js
@@ -1,4 +1,4 @@
-const ExplorerService = require("./../Services/ExplorerServices");
+const ExplorerController = require("./../controllers/ExplorerController");
 const express = require("express");
 const app = express();
 app.use(express.json());
@@ -10,4 +10,16 @@ app.get("/", (request, response) => {
 
 app.listen(port, () => {
     console.log(`FizzBuzz API in localhost:${port}`);
+});
+app.get("/v1/students/students", (request, response) => {
+    const listStudents = ExplorerController.allStudents();
+    response.json({Lista:listStudents});
+});
+app.get("/v1/students/certification/email", (request, response) => {
+    const listEmail = ExplorerController.emailWithCertification(true);
+    response.json({Lista: listEmail});
+});
+app.get("/v1/students/credits/above500", (request, response) =>{
+    const listAbove500 = ExplorerController.above500Students(500);
+    response.json({Lista: listAbove500});
 });
