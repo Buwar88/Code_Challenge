08e615caed8ff60f7bd3e4dbd9fad12090e3702c
Buwar88 <buwar_emerica@hotmail.com>
2022-05-04 00:34:43 -0500

    limpiando

--- index.js
+++
@@ -1,7 +0,0 @@
-const ExplorerService = require("./app/Services/ExplorerServices");
-const Reader = require("./app/utils/Reader");
-const students = Reader.readJsonFile("students.json")
-
-const totalStudents = ExplorerService.allStudents(students)
-const names = totalStudents.map(student => student.name);
-console.log(names)
